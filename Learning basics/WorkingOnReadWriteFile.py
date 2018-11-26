3450621c4a48d594eeb1d010c2b46d54ad7fcfef
Qbason <kuba1095@gmail.com>
2018-11-26 23:15:10 +0100

    update working the code before add return

--- Learning basics/WorkingOnReadWriteFile.py
+++ Learning basics/WorkingOnReadWriteFile.py
@@ -8,7 +8,7 @@ class Work_on_file():
     def write(self,data):
         #Write something to file 
         try:
-            file = open(    self.name_file    ,"a")
+            file = open(    self.name_file    ,"w")
             file.write( str(data)   )
         finally:
             file.close()
@@ -18,41 +18,48 @@ class Work_on_file():
 
         # 1 way read whole content
         with open(self.name_file,"r") as f:
+            self.content = ''
             try:
-                content = f.read(howmuchbytes)
-                print(content)
+                self.content = f.read(howmuchbytes)
             finally:
                 f.close()
+                return self.content
 
     def read_line_by_line(self):
         # 2 way read line by line
+        self.lines = []
         with open("data.txt","r") as f:
             try:
                 for line in f:
-                    print(line)
+                    self.lines.append(line)
+                    #print(line)
                     
             finally:
                 f.close()
+                return self.lines
 
     def read_char_by_char(self):
         # 3 way read char to char basic on 1 way ( read whole content)
         with open("data.txt","r") as f:
+            self.chars = []
             try:
                 content = f.read()
                 for char in content:
-                    print(char)
+                    #print(char)
+                    self.chars.append(char)
             except:
                 print("Error read char by char")
             finally:
                 f.close()
+                return self.chars
 
 
 data = Work_on_file()
 data.write("content \nsomething more \nthen that")
-data.read_content()
-#data.read_line_by_line()
-#data.read_char_by_char()
 
+print(data.read_content())
+print(data.read_line_by_line())
+print(data.read_char_by_char())
 
 
 
