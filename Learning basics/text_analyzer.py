7d39e0e9ca67ef34fba78b9c26907c300f2bb898
Qbason <kuba1095@gmail.com>
2018-11-29 00:50:55 +0100

    text analyzer simple counting letter

---
+++ Learning basics/text_analyzer.py
@@ -0,0 +1,63 @@
+
+""" counting the amount of specific letter """
+
+#Base
+def define_variable(letters):
+
+    
+    letters_array = letters.split(" ")
+    letters_object = {}
+
+    for letter in letters_array:
+        #letters_object[letter] = 0
+        letters_object.update({letter: 0})
+
+    #alphabet object
+    return letters_object
+
+
+# read from file
+def open_file(name = "text.txt"):
+
+    with open(name) as f:
+        content  = f.read()
+    # I do not have to close file
+    return(content)
+
+
+
+
+def counting(alphabet_object,content):
+#counting ...checking that the letter in text is the letter in the object
+    #save information about length in dictionary
+    alphabet_object['length'] = len(content)
+    for char in content:
+        for letter in alphabet_object:
+            if(char == letter):
+                alphabet_object[letter] +=1
+                break
+    return alphabet_object
+
+#showing information
+
+
+def show_information(letters_dictionary):
+    
+    length_content = letters_dictionary['length']
+
+    for letter in letters_dictionary:
+        proportion = (letters_dictionary[letter]*100/length_content)
+        proportion = round(proportion,2)
+        print("Letter {} is the {:.2f} % of main text".format(letter,   proportion  ))
+        #{:.2f} - > in the case of problem with rounding
+        #the last letter "lenght" is a control sum of content
+
+
+letters = "a ą b c ć d e ę f g h i j k l ł m n ń o ó p r s ś t u w y z ź ż A Ą B C Ć D E Ę F G H I J K L Ł M N Ń O Ó P R S Ś T U W Y Z Ź Ż"
+
+alphabet = define_variable(letters) #declarate a dictionary of each letter and set up the default value to 0
+content = open_file()               # open kontent default text.txt
+counted_letters = counting(alphabet,content)    #return the dictionary, which contains a counted letters in content
+
+show_information(counted_letters)
+
