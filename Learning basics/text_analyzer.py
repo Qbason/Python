53e3a058d0f1a97ee52f66f77e162abf8485f580
Qbason <kuba1095@gmail.com>
2018-11-29 01:02:24 +0100

    correction in text analyzer, find bugs/empty content

--- Learning basics/text_analyzer.py
+++ Learning basics/text_analyzer.py
@@ -17,12 +17,18 @@ def define_variable(letters):
 
 
 # read from file
-def open_file(name = "text.txt"):
+def open_file(name = "textt.txt"):
 
-    with open(name) as f:
-        content  = f.read()
-    # I do not have to close file
-    return(content)
+    try:
+
+        with open(name) as f:
+            content  = f.read()
+        # I do not have to close file
+    except FileNotFoundError:
+        content = ''
+        print("Error -> I cannot read file, it doesn't exist")
+    finally:
+        return(content)
 
 
 
@@ -31,6 +37,7 @@ def counting(alphabet_object,content):
 #counting ...checking that the letter in text is the letter in the object
     #save information about length in dictionary
     alphabet_object['length'] = len(content)
+    
     for char in content:
         for letter in alphabet_object:
             if(char == letter):
@@ -45,12 +52,15 @@ def show_information(letters_dictionary):
     
     length_content = letters_dictionary['length']
 
-    for letter in letters_dictionary:
-        proportion = (letters_dictionary[letter]*100/length_content)
-        proportion = round(proportion,2)
-        print("Letter {} is the {:.2f} % of main text".format(letter,   proportion  ))
-        #{:.2f} - > in the case of problem with rounding
-        #the last letter "lenght" is a control sum of content
+    if(length_content>0):
+        for letter in letters_dictionary:
+            proportion = (letters_dictionary[letter]*100/length_content)
+            proportion = round(proportion,2)
+            print("Letter {} is the {:.2f} % of main text".format(letter,   proportion  ))
+            #{:.2f} - > in the case of problem with rounding
+            #the last letter "lenght" is a control sum of content
+    else:
+        print("Empty content")
 
 
 letters = "a ą b c ć d e ę f g h i j k l ł m n ń o ó p r s ś t u w y z ź ż A Ą B C Ć D E Ę F G H I J K L Ł M N Ń O Ó P R S Ś T U W Y Z Ź Ż"
